b3c9f8c273df16f3ff464b8295bc3166204cb26c
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-02-06 02:00:23 +0100

    add a searchInput func

--- challenges_with_chat/challenge.js
+++ challenges_with_chat/challenge.js
@@ -1,3 +1,38 @@
+/* Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4 */
+
+const searchInsert = (nums, target) => {
+  const indexOfTarget = nums.indexOf(target)
+  let affectedIndex = -1
+  if (indexOfTarget !== -1) {
+    return indexOfTarget
+  } else {
+    nums
+      .sort((a, b) => a - b)
+      .forEach((ele, idx) => {
+        if (target > ele) {
+          affectedIndex = idx + 1
+        }
+      })
+    if (affectedIndex !== -1) {
+      return affectedIndex
+    } else {
+      return nums.length
+    }
+  }
+}
+
+console.log(searchInsert([1, 3, 5, 6], 7))
 // seperate balls problem
 // @input = string
 // s = 100
