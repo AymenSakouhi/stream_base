a3e627a5799143a271128390dfa0f8bf0221c518
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-04-04 01:27:32 +0200

    adding a promise all func

--- challenges_with_chat/challenge.js
+++ challenges_with_chat/challenge.js
@@ -1,3 +1,32 @@
+/* Heres a good JS test. 
+  Show us how to execute 5 promises in parallel, 
+  then show how to execute sequentially
+*/
+
+const promy1 = new Promise((resolve) =>
+  setTimeout(() => {
+    console.log("promise 1")
+    resolve()
+  }, 1000),
+)
+
+const promy2 = new Promise((resolve) =>
+  setTimeout(() => {
+    console.log("promise 2")
+    resolve()
+  }, 2000),
+)
+// sequentially
+Promise.all([promy1, promy2]).then(() => {
+  console.log("i am done")
+})
+
+// all parralle
+Promise.allSettled([promy1, promy2]).then(() => {
+  console.log("i am done")
+})
+
+// console.log("==1==>", [1, 2, 3, 4].map(num => parseInt(num))
 //Write a function deepEqual(a, b) that returns true
 //if two objects are deeply equal.
 const deepEqual = (a, b) => {
@@ -9,11 +38,11 @@ const deepEqual = (a, b) => {
   if (keysOfA.length !== keysOfB.length) return false
   return keysOfA.every((key) => keysOfB.includes[key] && deepEqual(a[key], b[key]))
 }
-console.log(deepEqual({ a: 7 }, { a: 5 })) //false
+/* console.log(deepEqual({ a: 7 }, { a: 5 })) //false
 console.log(deepEqual(7, 7)) //true
 console.log(deepEqual([1, 2, 3], [1, "2", 3])) //false
 console.log(deepEqual([1, 2, 3], [1, 2, 3])) //true
-console.log(deepEqual({ a: 5 }, { a: 5 })) //true
+console.log(deepEqual({ a: 5 }, { a: 5 })) //true */
 /*
  * Flatten Multi Arrays
  * @params {[][]} arr - Nested Arrays
@@ -31,9 +60,9 @@ const Flatten = (arr) => {
 }
 
 // example of how api reducer function work
-console.log([1, 2, 3].reduce((acc, val) => acc + val, 0))
+// console.log([1, 2, 3].reduce((acc, val) => acc + val, 0))
 
-console.log(Flatten([[1, 2, [3]], 4, 5, [[[[[[[6], [7]]]]]]]]))
+// console.log(Flatten([[1, 2, [3]], 4, 5, [[[[[[[6], [7]]]]]]]]))
 /*
  * Debounce Function (Medium)
  * @params {Function} Fn - function to debounce
@@ -86,10 +115,10 @@ const isAnagram = (s, t) => {
   return `the words [${s},${t}] are${result ? " " : " not"} Anagram`
 }
 
-console.log(isAnagram("anagram", "gramana"))
+/* console.log(isAnagram("anagram", "gramana"))
 console.log(isAnagram("cat", "pat"))
 console.log(isAnagram("catt", "pat"))
-console.log(isAnagram("caat", "catt"))
+console.log(isAnagram("caat", "catt")) */
 
 /* Given a linked list, swap every two adjacent nodes and return its head.
 You must solve the problem without modifying the values
@@ -115,7 +144,7 @@ const swapPairs = (head) => {
   return second
 }
 const list = new LinkedList([1, 2, 3, 4])
-console.log(swapPairs([1, 2, 3, 4]))
+// console.log(swapPairs([1, 2, 3, 4]))
 
 /* Given an integer x, return true if x is a 
 palindrome
@@ -134,8 +163,8 @@ const isPalindrome = (input) => {
   return input.toString().split("").reverse().join("") === input.toString()
 }
 
-console.log(isPalindrome("121")) // true
-console.log(isPalindrome("-121")) //false
+/* console.log(isPalindrome("121")) // true
+console.log(isPalindrome("-121")) //false */
 /* Given an array nums of n integers, 
   return an array of all the unique 
 quadruplets [nums[a], nums[b], nums[c], nums[d]] such that */
@@ -186,7 +215,7 @@ const fourSum = (nums, target) => {
   return result
 }
 
-console.log(fourSum([1, 0, -1, 0, -2, 2], 0))
+// console.log(fourSum([1, 0, -1, 0, -2, 2], 0))
 /* Example 1:
 
 Input: nums = [1,3,5,6], target = 5
@@ -221,7 +250,7 @@ const searchInsert = (nums, target) => {
   }
 }
 
-console.log(searchInsert([1, 3, 5, 6], 7))
+// console.log(searchInsert([1, 3, 5, 6], 7))
 // seperate balls problem
 // @input = string
 // s = 100
@@ -251,7 +280,7 @@ var minimumSteps = function (s) {
   return ans
 }
 
-console.log(minimumSteps("100"))
+// console.log(minimumSteps("100"))
 // ----- my approach
 const swapBlackBalls = (s) => {
   let left = 0
@@ -278,7 +307,7 @@ const swapBlackBalls = (s) => {
   return swaps
 }
 
-console.log(swapBlackBalls("100"))
+// console.log(swapBlackBalls("100"))
 
 // Thanks to BStudio for the interview question
 function countOnes(to) {
@@ -297,8 +326,8 @@ function countOnes(to) {
   return count
 }
 
-console.log(countOnes(13))
-console.log("----")
+// console.log(countOnes(13))
+// console.log("----")
 
 // Input: text = "AI is fun. AI is the future."
 // Output: { ai: 2, is: 2, fun: 1, the: 1, future: 1 }
@@ -314,7 +343,7 @@ const textRepeatCounter = (str) => {
     return freq
   }, {})
 }
-console.log(textRepeatCounter("AI is fun, AI is the future."))
+// console.log(textRepeatCounter("AI is fun, AI is the future."))
 
 // Longest Substring Without Repeating Characters
 //  Input: s = "abcabcbb"
@@ -335,7 +364,7 @@ const longestSubstring = (s) => {
   return maxLength
 }
 
-console.log(longestSubstring("abcabcbb"))
+// console.log(longestSubstring("abcabcbb"))
 
 // 8. Count Primes
 // Goal: Count the number of prime numbers
@@ -355,7 +384,7 @@ const countPrimes = (n) => {
   // return true
 }
 // Example
-console.log(countPrimes(20)) // Output: 4
+// console.log(countPrimes(20)) // Output: 4
 // How Map Works in JavaScript
 /* A Map is a collection of key-value pairs. 
  * Keys can be of any data type, 
@@ -368,17 +397,17 @@ map.has(key) – Checks if the key exists.
 map.delete(key) – Deletes a key-value pair.
 map.size – Returns the number of key-value pairs. */
 
-console.log(typeof null) // output: Object
-console.log(typeof 1) // output: Number
-console.log(typeof []) // object
-console.log(typeof {}) // object
+// console.log(typeof null) // output: Object
+// console.log(typeof 1) // output: Number
+// console.log(typeof []) // object
+// console.log(typeof {}) // object
 
 const fruits = ["banana", "strawberry", "banana", "mango", "strawberry"]
 const fruitsCount = new Map()
 for (const fruit of fruits) {
   fruitsCount.set(fruit, (fruitsCount.get(fruit) || 0) + 1)
 }
-console.log(fruitsCount)
+// console.log(fruitsCount)
 
 const numbers = [1, 2, 2, 3, 3, 4]
 // const numbersWithoutDupes = [...new Set(numbers)]
@@ -415,8 +444,9 @@ const isValid = (s) => {
   }
   return stack.length === 0
 }
+
 // Example
-console.log(isValid("()[]{}")) // Output: true
+// console.log(isValid("()[]{}")) // Output: true
 // 5. Find the Missing Number
 // Goal: Find the missing number in an array of size n containing numbers from 0 to n.
 const missingNumber = (nums) => {
@@ -426,7 +456,7 @@ const missingNumber = (nums) => {
   return total - sum
 }
 
-console.log(missingNumber([2, 3, 0, 1, 5])) // Output: 2
+// console.log(missingNumber([2, 3, 0, 1, 5])) // Output: 2
 // 9. Remove Duplicates from Sorted Array
 // Goal: Remove duplicates from a sorted array in-place and return the length of the modified array.
 const removeDuplicates = (arr) => {
@@ -434,7 +464,7 @@ const removeDuplicates = (arr) => {
   return [...new Set(arr)]
 }
 // Example
-console.log(removeDuplicates([1, 1, 2])) // Output: 2 (array becomes [1, 2])
+// console.log(removeDuplicates([1, 1, 2])) // Output: 2 (array becomes [1, 2])
 // 6. Merge Two Sorted Arrays
 // Goal: Merge two sorted arrays
 // into one sorted array.
@@ -445,17 +475,17 @@ const mergeSortedArrays = (nums1, nums2) => {
   return bothArrays.sort((a, b) => b - a)
 }
 // Example
-console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])) // Output: [1, 2, 3, 4, 5, 6]
+// console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])) // Output: [1, 2, 3, 4, 5, 6]
 
 // how to sum up an arr in js
 const arr = [1, 2, 3]
 const sum = arr.reduce((acc, curr) => acc + curr, 0)
 const rum = arr.reduce((acc, curr) => acc - curr, 0)
-console.log(["a", "b", "c"].reduce((acc, curr) => acc + " " + curr, ""))
+// console.log(["a", "b", "c"].reduce((acc, curr) => acc + " " + curr, ""))
 arr.sum
 
-console.log(sum, rum)
-console.log(arr.some((ele) => ele === 2))
+// console.log(sum, rum)
+// console.log(arr.some((ele) => ele === 2))
 
 // 10. Find the Intersection of Two Arrays
 // Goal: Find the intersection of two arrays
@@ -467,7 +497,7 @@ const intersection = (nums1, nums2) => {
 }
 //
 // Example
-console.log(intersection([1, 2, 2, 1], [1, 2])) // Output: [2]
+// console.log(intersection([1, 2, 2, 1], [1, 2])) // Output: [2]
 // 1. Reverse a String
 // Goal: Reverse the given string
 // and return the reversed version.
@@ -476,7 +506,7 @@ const reverseString = (s) => {
   return s.split("").reverse().join("")
 }
 // Example
-console.log(reverseString("hello")) // Output: "olleh"
+// console.log(reverseString("hello")) // Output: "olleh"
 
 const twoSum = (nums, target) => {
   const map = new Map()
@@ -513,18 +543,18 @@ const fizzBuzz = (num) => {
 }
 
 // Example
-console.log(fizzBuzz(15)) // Output: ["1", "2", "Fizz", ..., "FizzBuzz"]
+// console.log(fizzBuzz(15)) // Output: ["1", "2", "Fizz", ..., "FizzBuzz"]
 // 2. Two Sum
 // Goal: Find two numbers in the array
 // that add up to the target and return their indices.
 
 // Example
-console.log(twoSum([2, 7, 11, 15], 9)) // Output: [0, 1]
+// console.log(twoSum([2, 7, 11, 15], 9)) // Output: [0, 1]
 // 4. Valid Parentheses
 // Goal: Check if a string with parentheses, brackets, and braces is valid (properly closed and nested).
 
 // Example
-console.log(isValid("()[]{}")) // Output: true
+// console.log(isValid("()[]{}")) // Output: true
 
 // Example
 
